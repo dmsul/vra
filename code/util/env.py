dc9960907fe735c88b19a633df806dc26dd637b5
Daniel Sullivan <sullydm@gmail.com>
2016-12-06 17:07:56 -0500

    Add `out_path`

--- code/util/env.py
+++ code/util/env.py
@@ -1,4 +1,16 @@
 import os
 
+cwd = os.getcwd()
+
+if 'sullivan' in cwd or 'daniel' in cwd:
+    DROPBOX = os.path.expanduser('~/Dropbox')
+    ROOT_ON_DB = os.path.join(DROPBOX, 'research', 'vra')
+else:
+    raise NotImplementedError
+
+
 def data_path(*args):
     return os.path.join('..', 'data', *args)
+
+def out_path(*args):
+    return os.path.join(ROOT_ON_DB, 'out', *args)
