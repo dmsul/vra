f6ef89162ff5edf288ceff1d20d778782d3d64ef
Daniel Sullivan <sullydm@gmail.com>
2016-12-06 17:07:47 -0500

    Add style!

---
+++ code/util/style.py
@@ -0,0 +1,7 @@
+from __future__ import division
+
+from matplotlib.colors import rgb2hex
+
+navy = rgb2hex([x / 255 for x in (26, 71, 111)])
+maroon = rgb2hex([x / 255 for x in (144, 53, 59)])
+green = rgb2hex([x / 255 for x in (85, 117, 47)])
