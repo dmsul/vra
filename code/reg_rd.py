75b3ab13b3a2fb83d0cc3dec2d20ccc450db0c4d
Daniel Sullivan <sullydm@gmail.com>
2017-04-05 12:54:19 -0400

    minor changes (squash?)

--- code/reg_rd.py
+++ code/reg_rd.py
@@ -3,7 +3,7 @@ from __future__ import division, print_function, absolute_import
 import econtools.metrics as mt
 from econtools import save_cli
 
-from frd_scatter import frd_data
+from plot_rd import frd_data
 
 
 def plot_frd(year0, yearT):
@@ -12,13 +12,13 @@ def plot_frd(year0, yearT):
     diff_mean = df['diff'].mean()
     df['diff'] -= diff_mean
 
-    res1 = mt.reg(df, 'diff', ['run', 'low', 'run_low'],
-                  addcons=True, vce_type='robust')
-    print(res1.summary)
+    res = mt.reg(df, 'diff', ['low', 'run', 'run_low', 'pct_white'],
+                 addcons=True, vce_type='robust')
+    print(res.summary)
 
-    return df
+    return res
 
 
 if __name__ == "__main__":
     save = save_cli()
-    plot_frd(1960, 1968)
+    plot_frd(2012, 2016)
